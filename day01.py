f531ddce473be89d6cf0a19246270f72fcf351ec
Erick Wilts <erick.wilts@inergy.nl>
2022-12-01 15:08:46 +0100

    day 1

---
+++ day01.py
@@ -0,0 +1,15 @@
+sums = []
+curr_sum = 0
+with open("day01.txt") as lines:
+    for line in lines:
+        if not line.strip():
+            sums.append(curr_sum)
+            curr_sum = 0
+            continue
+        curr_sum += int(line)
+        sums.append(curr_sum)
+    sums.append(curr_sum)
+
+sums = sorted(sums)
+print("part 1:", sums[-1])
+print("part 2:", sum(sums[-3:]))
