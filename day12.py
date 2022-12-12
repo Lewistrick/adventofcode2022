04668ffadb0b4bc3764813f549b3911067649e2f
Lewistrick <erickwilts@gmail.com>
2022-12-12 19:18:35 +0100

    Solved day 12

--- day12.py
+++ day12.py
@@ -1,5 +1,6 @@
 # Day: 2022-12-12
 
+from collections import defaultdict
 from helpers import DIRS4, read_grid
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,6 +13,8 @@ logger.add(sys.stdout, level="DEBUG")
 Position = tuple[int, int]
 Grid = list[list[int]]
 
+PLOT = False
+
 
 def get_dist_grid(
     grid: Grid,
@@ -22,7 +25,7 @@ def get_dist_grid(
 
     Args:
         grid (list[list[int]]): The grid (height map) to search in.
-        finish (tuple[int, int]): The ending position.
+        finish (tuple[int, int]): The target position.
         seen (set[tuple[int, int]], optional): The positions visted. Defaults to None.
     """
     if seen is None:
@@ -31,30 +34,38 @@ def get_dist_grid(
     # create a zeroes grid to fill with distances
     distgrid = np.zeros((len(grid), len(grid[0])), dtype=int)
 
-    xfin, yfin = finish
-    distgrid[yfin][xfin] = 0  # the finish is 0 steps away from itself
+    x0, y0 = finish
     seen.add(finish)
 
-    expand = set()
-    expand.add(finish)
+    expand = defaultdict(set)  # for each distance, the positions to expand from
+    expand[0].add(finish)
 
     while expand:
-        currx, curry = expand.pop()
-        logger.debug(f"Expanding from ({currx}, {curry})")
-        currval = grid[curry][currx]
-        for dx, dy in DIRS4:
-            x, y = currx + dx, curry + dy
-            if (x, y) in seen:
-                continue
-            # newval should be currval-1 or currval
-            newval = grid[y][x]
-            if newval == currval - 1 or newval == currval:
-                logger.debug(f"Adding ({x}, {y}) to seen")
-                distgrid[y][x] = distgrid[curry][currx] + 1
-                seen.add((x, y))
-                expand.add((x, y))
+        # get the smallest distance and remove it from the expand dict
+        dist = min(expand.keys())
+        positions = expand.pop(dist)
 
-    return distgrid
+        # for each position, expand to all neighbours
+        for xold, yold in positions:
+            for dx, dy in DIRS4:
+                xnew, ynew = xold + dx, yold + dy
+                if (xnew, ynew) in seen:
+                    continue
+                if not (0 <= xnew < len(grid[0]) and 0 <= ynew < len(grid)):
+                    continue
+
+                highval = grid[yold][xold]
+                lowval = grid[ynew][xnew]
+
+                # highval can be at most 1 higher than lowval
+                if highval > lowval + 1:
+                    continue
+
+                expand[dist + 1].add((xnew, ynew))
+                seen.add((xnew, ynew))
+                distgrid[ynew][xnew] = dist + 1
+
+    return distgrid, seen
 
 
 startpos = None
@@ -70,15 +81,33 @@ for y, row in enumerate(strgrid):
             currow.append(0)
         elif c == "E":
             endpos = (x, y)
-            currow.append(26)
+            currow.append(25)
         else:
-            # it's a lowercase letter
+            # we know that it's a lowercase letter so we can convert it to an int
             currow.append(ord(c) - ord("a"))
     intgrid.append(currow)
     currow = []
 
-distgrid = get_dist_grid(intgrid, endpos)
+distgrid, tiles_seen = get_dist_grid(intgrid, endpos)
 
-# plot grid as image
-plt.imshow(distgrid, cmap="hot", interpolation="nearest")
-plt.show()
+if PLOT:
+    # plot grid as image
+    plt.imshow(distgrid, cmap="hot", interpolation="nearest")
+
+    # show the letters on the grid
+    for y, row in enumerate(strgrid):
+        for x, c in enumerate(row):
+            plt.text(x, y, c, color="blue", ha="center", va="center")
+
+    plt.show()
+
+part1 = distgrid[startpos[1]][startpos[0]]
+print(f"Part 1: {part1}")
+
+# find all positions with 'a'
+lowpos = np.argwhere(np.array(strgrid) == "a")
+
+# find the distances to all of these positions
+lowpos_values = [distgrid[y][x] for y, x in lowpos]
+lowpos_values_filter = [v for v in lowpos_values if v > 0]
+print(f"Part 2: {min(lowpos_values_filter)}")
